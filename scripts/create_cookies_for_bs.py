cf9e96a0bda70397c5c169c7b763cb1d8055442b
tam <tamco@list.ru>
2020-06-11 10:19:12 +0500

    move files in folder scripts

---
+++ scripts/create_cookies_for_bs.py
@@ -0,0 +1,37 @@
+'''
+Create cookies for your session (../data/cookies.json).
+
+It is not magic!!!
+1) creating session using the requests.
+2) using method session.post() with arguments: data(payload), headers(headers).
+3) then we take the cookies and write them to a file cookies.json
+'''
+
+
+import requests, json
+
+
+def create_cookies_for_bs(path='../data/cookies.json', Login, Password):
+	session = requests.Session()
+
+
+	# do login
+
+	headers = {
+		'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.116 Safari/537.36 OPR/67.0.3575.130'
+	}
+
+
+	payload = {
+		# POST data
+
+		'Login': Login,
+		'Password': Password,
+		'RememberMe': 'true'
+	}
+
+
+	session.post('https://author.today/account/login', data=payload, headers=headers)
+
+	with open(path, 'w') as f:
+	    json.dump(requests.utils.dict_from_cookiejar(session.cookies), f)
---
+++ scripts/create_data_json.py
@@ -0,0 +1,44 @@
+#  Author.Today parser
+#  create by tamco6
+#  
+from bs4 import BeautifulSoup
+import requests
+import json
+import modules
+
+
+def create_data_json(path_cookies='../data/cookies.json', path_data='../data/data.json'):
+	# create session
+
+	session = requests.Session()
+
+
+	# apply your cookies from data folder
+
+	with open(path_cookies) as f:
+	    session.cookies.update(json.load(f))
+
+	def get_info():
+		# get link on "My library" in author.today
+
+		page_source = session.get('https://author.today/').content
+
+		libraryLink = modules.get_link_on_library(page_source)
+
+		# get information for library
+
+		response = session.get(libraryLink)
+		soup = BeautifulSoup(response.content, 'html.parser')
+
+		datadict = modules.get_information_from_library(soup)
+
+
+		print(datadict)
+		# write info for library in json file
+		with open(path_data, 'w', encoding='utf-8') as f:
+			json.dump(datadict, f)
+
+
+
+	get_info()
+
